f99b8ecdc9c4690e47ad0d7fba4f98039dd7df42
superr-fluous <roman.solovev.w@gmail.com>
2025-03-18 21:36:58 +0300

    prettier config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,8 +5,31 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.stylua.with({ filetypes = { "lua" } }),
+				null_ls.builtins.formatting.prettier.with({
+					-- https://github.com/MunifTanjim/prettier.nvim
+					bin = "prettierd",
+					filetypes = {
+						"javascript",
+						"typescript",
+						"javascriptreact",
+						"typescriptreact",
+						"css",
+						"html",
+						"markdown",
+						"json",
+						"less",
+						"css",
+						"scss",
+					},
+					cli_options = {
+						config_precedence = "prefer-file",
+						tab_width = 2,
+						trailing_comma = "es5",
+						use_tabs = true,
+						print_width = 120,
+					},
+				}),
 				require("none-ls.diagnostics.eslint_d"),
 				require("none-ls.formatting.eslint_d"),
 			},
